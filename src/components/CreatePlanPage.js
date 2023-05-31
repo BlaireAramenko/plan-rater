d52e8b34af0aa040696ee3195cee5352f2a79392
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-05-31 13:50:18 -0700

    add plan feature

--- src/components/CreatePlanPage.js
+++ src/components/CreatePlanPage.js
@@ -1,8 +1,59 @@
+import { useState } from "react";
 import React from 'react';
 
 const CreatePlanPage = () => {
+
+  //state hook - 'useState'
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  //helper functions
+
+  function addItem() {
+
+    if (!newItem) {
+      alert("Enter an item.")
+      return;
+    }
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+    setItems(oldList => [...oldList, item]);
+
+    setNewItem("");
+
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
+
   return (
+    <div className="CreatePlanPage">
+
     <h1>CreatePlanPage</h1>
+
+    <input
+    type="text"
+    placeholder='Add a plan'
+    value={newItem}
+    onChange={e => setNewItem(e.target.value)}
+    />
+
+    <button onClick={() => addItem()}>Add</button>
+
+    <ul>
+      {items.map(item => {
+        return(
+          <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}>❌</button></li>
+        )
+      })}
+    </ul>
+
+    </div>
   );
 };
 
